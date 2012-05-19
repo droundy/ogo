bc1c4156305e891b25154aec95ea18e06b4d3f9c
David Roundy <roundyd@physics.oregonstate.edu>
2012-05-19 06:14:44 -0700

    import imports recursively

--- ogo.go
+++ ogo.go
@@ -5,6 +5,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"go/build"
 	"go/ast"
 	"go/parser"
@@ -12,35 +13,64 @@ import (
 	"go/printer"
 )
 
-func importFile(imports map[string]*ast.Object, path string) (pkg *ast.Object, err error) {
-	if p, ok := imports[path]; ok {
-		return p, nil
+func parseFile(fset *token.FileSet, srcdir, f string) (parsedf *ast.File, err error) {
+	parsedf, err = parser.ParseFile(fset, filepath.Join(srcdir,f), nil, parser.ParseComments)
+	if err != nil {
+		return
 	}
-
-	x, err := build.Import(path, ".", 0)
-	if err != nil { return }
-
-	var fset token.FileSet
-	files := make(map[string]*ast.File)
-	for _,f := range x.GoFiles {
-		parsedf, err := parser.ParseFile(&fset, f, nil, parser.ParseComments)
-		if err != nil {
-			fmt.Println("error on file", f, err)
-			return nil, err
+	fmt.Println("**********************")
+	fmt.Println(f)
+	fmt.Println("**********************")
+	fmt.Println(parsedf)
+	fmt.Println("**********************")
+	printer.Fprint(os.Stdout, fset, parsedf)
+	fmt.Println("**********************")
+	//fmt.Println(parsedf.Unresolved)
+	newunresolved := make([]*ast.Ident, 0, len(parsedf.Unresolved))
+	for _,i := range parsedf.Unresolved {
+		switch i.Name {
+		case "string", "nil", "int", "bool", "byte", "uint", "error",
+			"iota",
+			"panic", "recover", "make", "len", "append", "new", "print",
+			"false", "true":
+		default:
+			newunresolved = append(newunresolved, i)
 		}
-		files[f] = parsedf
-		fmt.Println("**********************")
-		fmt.Println(f)
-		fmt.Println("**********************")
-		printer.Fprint(os.Stdout, &fset, parsedf)
-		fmt.Println("**********************")
 	}
+	parsedf.Unresolved = newunresolved
+	fmt.Println(parsedf.Unresolved)
+	return
+}
 
-	scope := ast.NewScope(nil)
-	p, err := ast.NewPackage(&fset, files, importFile, scope)
-	pkg = ast.NewObj(ast.Pkg, p.Name)
-	pkg.Data = scope
-	return pkg, err
+func importPath(packages map[string](map[string]*ast.File), fset *token.FileSet, path string) (fmap map[string]*ast.File, err error) {
+	if _,ok := packages[path]; !ok {
+		fmt.Println("Path is:", path)
+		x, err := build.Import(path, ".", 0)
+		if err != nil {
+			return fmap, err
+		}
+		fmap = make(map[string]*ast.File)
+		for _,f := range x.GoFiles {
+			fmt.Println("Looking up", f, "for import", path, "in directory", x.Dir)
+			parsedf, err := parseFile(fset, x.Dir, f)
+			if err != nil {
+				fmt.Println("error on file", f, err)
+			} else {
+				fmap[f] = parsedf
+			}
+		}
+		packages[path] = fmap
+	}
+	fmap = packages[path]
+
+	for _,f := range packages[path] {
+		for _,i := range f.Imports {
+			subpath := i.Path.Value[1:len(i.Path.Value)-1]
+			packages[subpath], err = importPath(packages, fset, subpath)
+			if err != nil { panic(err) }
+		}
+	}
+	return
 }
 
 func main() {
@@ -51,46 +81,24 @@ func main() {
 	fmt.Println(*x)
 	fmt.Println("gofiles:", x.GoFiles)
 	fmt.Println(x.IsCommand())
+	if !x.IsCommand() {
+		fmt.Println("Use ogo on commands only!")
+		os.Exit(1)
+	}
 
 	var fset token.FileSet
-	files := make(map[string]*ast.File)
+	packages := make(map[string](map[string]*ast.File))
+	packages["main"] = make(map[string]*ast.File)
 	for _,f := range x.GoFiles {
-		parsedf, err := parser.ParseFile(&fset, f, nil, parser.ParseComments)
+		parsedf, err := parseFile(&fset, ".", f)
 		if err != nil {
 			fmt.Println("error on file", f, err)
 		} else {
-			files[f] = parsedf
-			fmt.Println("**********************")
-			fmt.Println(f)
-			fmt.Println("**********************")
-			fmt.Println(parsedf)
-			fmt.Println("**********************")
-			printer.Fprint(os.Stdout, &fset, parsedf)
-			fmt.Println("**********************")
-			fmt.Println(parsedf.Unresolved)
-			newunresolved := make([]*ast.Ident, 0, len(parsedf.Unresolved))
-			for _,i := range parsedf.Unresolved {
-				switch i.Name {
-				case "string", "nil", "panic", "make":
-				default:
-					newunresolved = append(newunresolved, i)
-				}
-			}
-			parsedf.Unresolved = newunresolved
-			fmt.Println(parsedf.Unresolved)
+			packages["main"][f] = parsedf
 		}
 	}
-	scope := ast.NewScope(nil)
-	p, err := ast.NewPackage(&fset, files, nil, scope)
+	_, err = importPath(packages, &fset, "main")
 	if err != nil {
-		fmt.Println("error on NewPackage:", err)
-	}
-	for i,_ := range p.Imports {
-		fmt.Println("Imported: ", i)
-		//fmt.Println(i, o.Data.(*ast.Scope).Objects)
-	}
-	for i,f := range p.Files {
-		fmt.Println("Files: ", i)
-		printer.Fprint(os.Stdout, &fset, f)
+		fmt.Println("Error importing stuff:", err)
 	}
 }
